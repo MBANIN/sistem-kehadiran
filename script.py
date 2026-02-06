80edf108c8fe53ba932025120790c6277b5acea8
muhdbanin05@gmail.com <23225179@adtec.edu.my>
2026-02-06 11:12:56 +0800

    Initial commit: Pembangunan Sistem Kehadiran

---
+++ script.py
@@ -0,0 +1,22 @@
+import os
+
+def test():
+    file_name = "index.html"
+    print(f"Memulakan semakan pada: {file_name}")
+
+    if os.path.exists(file_name):
+        print("PENGESAHAN: Fail index.html ditemui.")
+    else:
+        print("RALAT: Fail index.html tidak dijumpai!")
+        exit(1)
+
+    with open(file_name, "r") as f:
+        kandungan = f.read()
+        if "alert(" in kandungan:
+            print("PENGESAHAN: Fungsi alert dikesan dalam kod.")
+        else:
+            print("RALAT: Fungsi alert tidak dijumpai!")
+            exit(1)
+
+if __name__ == "__main__":
+    test()
